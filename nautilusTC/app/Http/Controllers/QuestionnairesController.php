6d5d716fe58ea2b20baa7fde63bbd82b8179eb47
Eric Torres <eftr249@outlook.com>
2023-04-08 21:52:03 -0600

    Configración base de datos

---
+++ nautilusTC/app/Http/Controllers/QuestionnairesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Questionnaires;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QuestionnairesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Questionnaires $questionnaires)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Questionnaires $questionnaires)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Questionnaires $questionnaires)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Questionnaires $questionnaires)
+    {
+        //
+    }
+}
--- nautilusTC/database/migrations/2014_10_12_000000_create_users_table.php
+++ nautilusTC/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('consent_information')->default(false);
+            $table->boolean('consent_practices')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
